5f3220a20a4acde09821df4fc633eb725fd6b072
Vyacheslav <barikedoo@gmail.com>
2017-06-12 12:53:43 +0300

    get in touch section done

--- source/js/main.js
+++ source/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $('.slider').slick({
     dots:true,
     prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><</button>',
-    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button">></button>',
+    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
     customPaging: function(slider, i) {
     return $('<button type="button" data-role="none" role="button" tabindex="0" />');
     },
@@ -17,7 +17,7 @@ $(document).ready(function(){
     // Give slider arrows .slider-button class
     $('.slick-arrow').addClass('slider-button');
 
-
+    // Form validation
     $("#commentForm").validate();
     
 });
