f63ecaceedad5f69a5e1830d929663fb42284214
Vyacheslav <barikedoo@gmail.com>
2017-06-14 22:22:58 +0300

    main-content-show button added + validation fixed

--- source/js/main.js
+++ source/js/main.js
@@ -1,24 +1,44 @@
-$(document).ready(function(){
-    console.log('s');
+$(document).ready(function () {
 
-    // SLIDER
-    $('.slider').slick({
-    dots:true,
-    prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><</button>',
-    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
-    customPaging: function(slider, i) {
-    return $('<button type="button" data-role="none" role="button" tabindex="0" />');
+  // SLIDER
+  $('.slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><span class="chevron left"></button>',
+    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><span class="chevron right"></span></button>',
+    customPaging: function (slider, i) {
+      return $('<button type="button" data-role="none" role="button" tabindex="0" />');
     },
     appendArrows: $('.slider__nav'),
-    appendDots: $('.slider__nav')
+    appendDots: $('.slider__nav'),
   });
 
 
-    // Give slider arrows .slider-button class
-    $('.slick-arrow').addClass('slider-button');
+  // Give slider arrows .slider-button class
+  $('.slick-arrow').addClass('slider-button');
 
-    // Form validation
-    $("#commentForm").validate();
-    
+  // Form validation
+  $("#commentForm").validate({
+    errorPlacement: function(error, element) {
+    }
+  });
+
+  // Main content slideToggle
+  $('#js__contentShow').on('click', function () {
+
+    $(this).find('span').toggleClass('bottom top');
+
+    $('.main-content').slideToggle( 'slow' );
+
+    if ($('.chevron').hasClass('top')) {
+      $('html, body').animate({
+        scrollTop: $("#our_works").offset().top
+      }, 1000);
+      console.log('2');
+    }
+
+
+
+  });
+
 });
 
